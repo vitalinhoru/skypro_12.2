2153e79137f45327db9f5b19a5b89f37d428b11d
vitalinho <kirillovitaly@mail.ru>
2023-09-24 12:44:52 +0300

    тесты дают 100% покрытие

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,5 @@ def test_get():
 def test_my_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], None, None) == []
+    assert arrs.my_slice([1, 2, 3, 4], None, 3) == [1, 2, 3]
